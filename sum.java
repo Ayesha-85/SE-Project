df1386418fbda0d1ee515dc7aab4100c39e9a77c
Deekshitha_Y <deekshithakmit@gmail.com>
2025-08-04 10:26:01 +0530

    trying to Add files after fork

--- sum.java
+++ sum.java
@@ -2,20 +2,17 @@ import java.util.Scanner;
 
 public class SumTwoNumbers {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in); // Create a Scanner object
+        Scanner input = new Scanner(System.in); 
 
-        // Prompt user for input
-        System.out.print("Enter the first number: ");
-        int num1 = input.nextInt(); // Read first number
+        // System.out.print("Enter the first number: ");
+        int num1 = input.nextInt(); 
 
-        System.out.print("Enter the second number: ");
-        int num2 = input.nextInt(); // Read second number
-
-        int sum = num1 + num2; // Calculate sum
-
-        // Display result
+        // System.out.print("Enter the second number: ");
+        int num2 = input.nextInt(); 
+        int sum = num1 + num2; 
+        
         System.out.println("The sum is: " + sum);
 
-        input.close(); // Close the Scanner
+        input.close(); 
     }
 }
